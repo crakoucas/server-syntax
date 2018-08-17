c7456d87a76d3feb9843899c040ec23acd065c73
renaud <renaud@crakoucas.ovh>
2018-08-17 12:54:44 +0200

    disable-powered by - helmet

--- src/app.js
+++ src/app.js
@@ -3,10 +3,13 @@ import bodyParser from "body-parser";
 import { graphqlExpress, graphiqlExpress } from "apollo-server-express";
 import schema from "./schema";
 import cors from "cors";
+import helmet from "helmet";
 const app = express();
 
 const port = process.env.PORT || 3000;
+app.disable("x-powered-by");
 app.use(cors());
+app.use(helmet());
 app.use("/graphql", bodyParser.json(), graphqlExpress({ schema }));
 app.use(
   "/graphiql",
